c3a94b84cbd30a8ae5a2c73c926fbfe3310d045c
Petar Blazevski <petar.blazevski@gmail.com>
2017-07-02 18:11:26 +0200

    Initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { RoutingComponent } from './routing/routing.component';
+import { SimpleRouteComponent } from './simple-route/simple-route.component';
+
+const appRoutes: Routes = [
+  { path: 'simple-route', component: SimpleRouteComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    RoutingComponent,
+    SimpleRouteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/simple-route/simple-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-route',
+  templateUrl: './simple-route.component.html',
+  styleUrls: ['./simple-route.component.css']
+})
+export class SimpleRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
